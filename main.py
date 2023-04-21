d68d66ad65f1a93cff9775f1f42f624808ca4e83
Victor Zuluaga <zrvictor@outlook.com>
2023-04-21 16:55:14 -0500

    query and path params validation with pydantic FastApi

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+import datetime
 
 app = FastAPI()
 app.title = "My first api with FastAPI"
@@ -49,10 +50,22 @@ class Movie(BaseModel):
   id: Optional[int] = None
   #Other way to do this
   #id: int | None = None
-  title: str
-  year: int
-  rating: float | int
+  title: str = Field(min_length=5, max_length=15)
+  year: int = Field(le=datetime.date.today().year)
+  rating: float | int = Field(ge=1, le=10)
   category: str
+
+  # Crear valores por defecto.
+  class Config():
+    schema_extra = {
+      "example": {
+        "id": 12,
+        "title": "The Godfather",
+        "year": 1972,
+        "rating": 8.5,
+        "category": "Action"
+      }
+    }
 
 @app.get('/', tags=["Home"])
 def message():
@@ -63,14 +76,14 @@ def get_movies():
   return movies
 
 @app.get('/movies/{id}', tags=["movies"])
-def get_movies(id: int):
+def get_movies(id: int = Path(ge=1, le=2000)):
   for item in movies:
     if item["id"] == id:
       return item
   return "There's no items to show"
 
 @app.get('/movies/', tags=["movies"])
-def get_movies_by_category(category: str, year: int): #Usando parametros query
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)): #Usando parametros query
   return [movie for movie in movies if movie['category'].lower() == category.lower()]
   #Another solution is:
   #return list(filter(lambda item: item['category'] == category , movies))
