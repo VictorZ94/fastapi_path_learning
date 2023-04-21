72f58894ab3e98b937e3dfecdc39475be2820e2c
Victor Zuluaga <zrvictor@outlook.com>
2023-04-21 17:38:39 -0500

    modularized data

---
+++ data.py
@@ -0,0 +1,37 @@
+movies = [
+    {
+      "id": 1,
+      "title": "The Godfather",
+      "year": 1972,
+      "rating": 8.5,
+      "category": "Action"
+    },
+    {
+      "id": 2,
+      "title": "The Godfather: Part II",
+      "year": 1974,
+      "rating": 8.5,
+      "category": "Sci Fic"
+    },
+    {
+      "id": 3,
+      "title": "The Godfather: Part III",
+      "year": 1975,
+      "rating": 8.5,
+      "category": "Thriller"
+    },
+    {
+      "id": 4,
+      "title": "The Godfather: Part IV",
+      "year": 1976,
+      "rating": 8.5,
+      "category": "Comedy"
+    },
+    {
+      "id": 5,
+      "title": "The Godfather: Part V",
+      "year": 1977,
+      "rating": 9.5,
+      "category": "Action"
+    }
+  ]
--- main.py
+++ main.py
@@ -2,48 +2,11 @@ from fastapi import FastAPI, Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
 from model import Movie
 from typing import List
+from data import movies
 
 app = FastAPI()
 app.title = "My first api with FastAPI"
 app.version = "0.0.1"
-
-movies = [
-    {
-      "id": 1,
-      "title": "The Godfather",
-      "year": 1972,
-      "rating": 8.5,
-      "category": "Action"
-    },
-    {
-      "id": 2,
-      "title": "The Godfather: Part II",
-      "year": 1974,
-      "rating": 8.5,
-      "category": "Sci Fic"
-    },
-    {
-      "id": 3,
-      "title": "The Godfather: Part III",
-      "year": 1975,
-      "rating": 8.5,
-      "category": "Thriller"
-    },
-    {
-      "id": 4,
-      "title": "The Godfather: Part IV",
-      "year": 1976,
-      "rating": 8.5,
-      "category": "Comedy"
-    },
-    {
-      "id": 5,
-      "title": "The Godfather: Part V",
-      "year": 1977,
-      "rating": 9.5,
-      "category": "Action"
-    }
-  ]
 
 @app.get('/', tags=["Home"])
 def message():
