7a4d4a055590ce1da4e3d3f37d0396b953e0c06d
Victor Zuluaga <zrvictor@outlook.com>
2023-04-22 16:34:05 -0500

    auth validation with middlewares

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse, JSONResponse
 from model import Movie, User
 from typing import List
 from data import movies
-from jwt_manager import create_token
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPBearer
 
 app = FastAPI()
 app.title = "My first api with FastAPI"
 app.version = "0.0.1"
+
+class JWTBearer(HTTPBearer):
+  async def __call__(self, request: Request):
+    auth = await super().__call__(request)
+    data = validate_token(auth.credentials)
+    if data['email'] != "admin":
+      raise HTTPException(status_code=403, detail="Forbidden")
+
 
 @app.get('/', tags=["Home"])
 def message():
@@ -20,11 +29,11 @@ def login(user: User):
     return JSONResponse(content=token)
   return JSONResponse({"message": "Invalid user or no authenticated"})
 
-@app.get('/movies', tags=["movies"], response_model=List[Movie], status_code=200)
+@app.get('/movies', tags=["movies"], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
   return JSONResponse(content=movies)
 
-@app.get('/movies/{id}', tags=["movies"], response_model=Movie, status_code=200)
+@app.get('/movies/{id}', tags=["movies"], response_model=Movie, status_code=200, dependencies=[Depends(JWTBearer())])
 def get_movies(id: int = Path(ge=1, le=2000)) -> Movie:
   for item in movies:
     if item["id"] == id:
